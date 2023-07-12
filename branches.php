b0ef3394a9543c53ca7c4ee9ea2dca7cb9e7a05d
Steve Boyd <emteknetnz@gmail.com>
2023-07-12 13:15:56 +1200

    .

--- branches.php
+++ branches.php
@@ -5,14 +5,5 @@ include 'funcs.php';
 $defaultBranch = getenv('DEFAULT_BRANCH');
 $minimumCmsMajor = getenv('MINIMUM_CMS_MAJOR');
 
-// using handler to echo a string on error/exception because bash is calling
-// this within the context of BRANCHES=$(php branches.php) which means that while the error/exception
-// will halt the github action, it will not echo the error/exception to the github action log
-// function handler(Throwable $exception) {
-//     echo "FAILURE - " , $exception->getMessage(), "\n";
-// }
-// set_exception_handler('handler');
-// set_error_handler('handler');
-
 $branches = branches($defaultBranch, $minimumCmsMajor);
 echo implode(' ', $branches);
