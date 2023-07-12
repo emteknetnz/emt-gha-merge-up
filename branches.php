d1cbda682a80032ad6e2e035319ee58d8a3bf96e
Steve Boyd <emteknetnz@gmail.com>
2023-07-12 12:52:55 +1200

    .

--- branches.php
+++ branches.php
@@ -5,4 +5,16 @@ include 'funcs.php';
 $defaultBranch = getenv('DEFAULT_BRANCH');
 $minimumCmsMajor = getenv('MINIMUM_CMS_MAJOR');
 
-echo implode(' ', versions($defaultBranch, $minimumCmsMajor));
+$branches = [];
+
+// using this try/catch block to echo a string on exception because bash is calling
+// this within the context of BRANCHES=$(php branches.php) which means that while the exception
+// will halt the github action, it will not echo the exception to the github action log
+try {
+    $branches = branches($defaultBranch, $minimumCmsMajor);
+} catch (Exception $e) {
+    echo 'EXCEPTION - ' . $e->getMessage();
+    exit(0);
+}
+
+echo implode(' ', $branches);
